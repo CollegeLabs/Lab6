d71597113f5b1c566e7920676b6f474fd2c12244
ConnorQR <5050771@nwpolytech.ca>
2025-10-30 09:38:01 -0600

    test 6

--- Lab6/Task2.py
+++ Lab6/Task2.py
@@ -40,7 +40,7 @@ def main():
 
             if st.button("Run Backtrack"):
                 backtracking_search(basicSudokuCSP)
-                buildGraph(basicSudokuCSP, nodeColors, False)
+                buildGraph(basicSudokuCSP, nodeColors, True)
             
         
         #st.button("Run AC-3", on_click= , args= [option])
