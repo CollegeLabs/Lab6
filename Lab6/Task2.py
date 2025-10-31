857697a30a68af549a4dc5c67f80408bf7f22643
ConnorQR <5050771@nwpolytech.ca>
2025-10-31 11:58:31 -0600

    how? why?

--- Lab6/Task2.py
+++ Lab6/Task2.py
@@ -21,26 +21,26 @@ nodeColors={
 
 
 def main():
-    #if "clicked" not in st.session_state:
-    #    st.session_state["clicked"] = False
+    if "clicked" not in st.session_state:
+        st.session_state["clicked"] = False
         
-    #if not st.session_state["clicked"]:
-        # Set header title
-    st.header("CSP: Simple Sudoku Example")
-    st.header("_Initial Sudoku._", divider=True)
+    if not st.session_state["clicked"]:
+        #Set header title
+        st.header("CSP: Simple Sudoku Example")
+        st.header("_Initial Sudoku._", divider=True)
         
-    sudokuNeighbors,sudokuDomains,sudokuConstraints1=getSudokuData()        
-    basicSudokuCSP=CSP(variables=sudokuNeighbors.keys(),neighbors=sudokuNeighbors, domains=sudokuDomains, constraints=sudokuConstraints1)
+        sudokuNeighbors,sudokuDomains,sudokuConstraints1=getSudokuData()        
+        basicSudokuCSP=CSP(variables=sudokuNeighbors.keys(),neighbors=sudokuNeighbors, domains=sudokuDomains, constraints=sudokuConstraints1)
 
-    buildGraph(basicSudokuCSP, nodeColors)
+        buildGraph(basicSudokuCSP, nodeColors)
         
     if st.button("Run AC-3"):
         AC3(basicSudokuCSP)
         buildGraph(basicSudokuCSP, nodeColors, True)
 
-        if st.button("Run Backtrack"):
-            backtracking_search(basicSudokuCSP)
-            buildGraph(basicSudokuCSP, nodeColors, True)
+    if st.button("Run Backtrack Search"):
+        backtracking_search(basicSudokuCSP)
+        buildGraph(basicSudokuCSP, nodeColors, False)
             
         
         #st.button("Run AC-3", on_click= , args= [option])
