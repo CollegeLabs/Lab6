cb5fed0529efe659c6357dfd4cf072bade416670
ConnorQR <5050771@nwpolytech.ca>
2025-10-30 23:25:38 -0600

    I think this is the last one

--- Lab6/Task2.py
+++ Lab6/Task2.py
@@ -140,6 +140,7 @@ def buildGraph(SudokuCSP, nodeColors, ac3=False):
         if ac3:
             if len(SudokuCSP.curr_domains[node])==1:
                 nodeColorsDict.setdefault(node,nodeColors["filled"])
+                nodeTitlesDict.setdefault(node,str(SudokuCSP.curr_domains[node][0]))
         if len(SudokuCSP.domains[node])==1:
             nodeColorsDict.setdefault(node,nodeColors["filled"])
             if ac3:
